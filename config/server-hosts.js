4a75f2e6b7433b09abbb695d1c7a2646ed9b02ad
bugme-not <d4em0n35@gmail.com>
2026-08-03 13:54:49 +0800

    Create server-hosts.js

---
+++ config/server-hosts.js
@@ -0,0 +1,12 @@
+const SERVER_HOST_CONFIG = {
+    MASTER_LIST_URL: 'https://test.run.app'
+    deployedServers: [
+        "cxlvin-22d733-2tb4hqsf2a-uc.a.run.app",
+        "cxlvin-2wd733-2tb4hqsf2a-uc.a.run.app",
+        "cxlvin-q2d733-2tb4hqsf2a-uc.a.run.app",
+        "cxlvin-629d3d-2tb4hqsf2a-uc.a.run.app",
+        "cxlvin-397082-2tb4hqsf2a-uc.a.run.app",
+        "cxlvin-07cab7-2tb4hqsf2a-uc.a.run.app",
+        "test"    
+    ]
+};
